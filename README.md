5d1aabaecf03617a8bcdc60ea2b85d3d09445c55
Steve Carey <steve981cr@gmail.com>
2018-08-08 01:26:15 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# README
+
+This application goes along with the Add JWT Authentication to a Rails and React application:
+
+Building the React front end: http://www.techandstartup.org/tutorials/rails-react-jwt
+
+This is a Rails API only application.
+
+The React app is in the client directory.
+
+To run this app locally, if you have Foreman installed on your system:
+
+foreman start -f Procfile.dev
+
+The app will launch on port 3000.
---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,50 @@
+class ArticlesController < ApplicationController
+  before_action :authenticate_user
+
+  # GET /articles
+  def index
+    @articles = Article.all
+
+    render json: @articles
+  end
+
+  # GET /articles/1
+  def show
+    @article = Article.find(params[:id])
+    render json: @article
+  end
+
+  # POST /articles
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      render json: @article, status: :created, location: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /articles/1
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      render json: @article
+    else
+      render json: @article.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /articles/1
+  def destroy
+    @article = Article.find(params[:id])
+    if current_user.admin?
+      @article.destroy
+    end
+  end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :content)
+    end
+end
